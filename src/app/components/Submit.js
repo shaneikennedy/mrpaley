2cd7c1ff096912fd3d9838392ddba6c1c632c0c9
Shane Kennedy <shane.kennedy19@gmail.com>
2024-03-12 10:22:25 +0100

    Shitty chat elipsis

--- src/app/components/Submit.js
+++ src/app/components/Submit.js
@@ -5,28 +5,37 @@ export function Submit({ inputDisabled, handleMessage }) {
   let [input, setInput] = useState("");
   const onChange = (e) => setInput(e.target.value);
   return (
-    <form
-      action={(data) => {
-        setInput("");
-        handleMessage(data);
-      }}
-      className="flex items-center rounded-b-lg bg-gray-700 px-4 py-2"
-    >
-      <input
-        name="userMsg"
-        type="text"
-        value={input}
-        onChange={onChange}
-        placeholder="Type your message..."
-        className="w-full rounded-full bg-gray-600 px-4 py-2 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      />
-      <button
-        type="submit"
-        disabled={inputDisabled}
-        className="ml-2 rounded-full bg-blue-500 px-4 py-2 text-white"
+    <div>
+      {inputDisabled && (
+        <div className="left-2 mb-2 ml-4 flex w-full text-gray-400">
+          <div className="mr-1 h-3 w-3 animate-bounce rounded-full bg-gray-400"></div>
+          <div className="mr-1 h-3 w-3 animate-bounce rounded-full bg-gray-400 delay-75"></div>
+          <div className="mr-1 h-3 w-3 animate-bounce rounded-full bg-gray-400 delay-150"></div>
+        </div>
+      )}
+      <form
+        action={(data) => {
+          setInput("");
+          handleMessage(data);
+        }}
+        className="flex items-center rounded-b-lg bg-gray-700 px-4 py-2"
       >
-        Send
-      </button>
-    </form>
+        <input
+          name="userMsg"
+          type="text"
+          value={input}
+          onChange={onChange}
+          placeholder="Type your message..."
+          className="w-full rounded-full bg-gray-600 px-4 py-2 text-gray-200 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          type="submit"
+          disabled={inputDisabled}
+          className="ml-2 rounded-full bg-blue-500 px-4 py-2 text-white"
+        >
+          Send
+        </button>
+      </form>
+    </div>
   );
 }
