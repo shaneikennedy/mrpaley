fc71e840238c79cce430e67d21c530d1adf0b8a0
Shane Kennedy <shane.kennedy19@gmail.com>
2024-03-12 08:08:51 +0100

    Give mr paley an avatar

--- src/app/components/PaleyMessage.js
+++ src/app/components/PaleyMessage.js
@@ -2,8 +2,8 @@ export function PaleyMessage({ message }) {
   return (
     <div className="flex items-start">
       <img
-        src="avatar.jpg"
-        alt="Avatar"
+        src="mrpaley.jpeg"
+        alt="Mr. Paley"
         className="mr-2 h-8 w-8 rounded-full"
       />
       <div className="rounded-lg bg-gray-600 px-4 py-2">
--- src/app/components/UserMessage.js
+++ src/app/components/UserMessage.js
@@ -4,11 +4,6 @@ export function UserMessage({ message }) {
       <div className="rounded-lg bg-blue-500 px-4 py-2 text-white">
         <p>{message}</p>
       </div>
-      <img
-        src="avatar.jpg"
-        alt="Avatar"
-        className="ml-2 h-8 w-8 rounded-full"
-      />
     </div>
   );
 }
